a23179ffa71aab0784a3bf8e5c6753a42e0f6dd3
fahledu <fahledu@gmail.com>
2024-10-30 19:50:01 -0300

    delegando resposabilidade de regras de negocio para service

--- src/services/players-services.ts
+++ src/services/players-services.ts
@@ -1,3 +1,12 @@
+import { noContent, ok } from "../utils/http-helper";
+
 export const getPlayerService = async () => {
-return { player: 'Ronaldo' }
+    const data = { player: 'Ronaldo' }
+    let response = null;
+    if (data){
+        response  = await ok(data);
+    }else{
+        response = await noContent();
+    }
+    return response
 }
--- src/utils/http-helper.ts
+++ src/utils/http-helper.ts
@@ -6,6 +6,13 @@ interface HttpResponse {
 export const ok = async (data: any): Promise<HttpResponse> => {
     return {
         statusCode: 200,
-        body: data
+        body: data,
     }
+}
+
+export const noContent = async (): Promise<HttpResponse> => {
+return{
+    statusCode: 204,
+    body: null,
+}
 }
